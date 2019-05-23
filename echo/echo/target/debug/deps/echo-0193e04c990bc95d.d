4853bda81a5465e674bc72480b95521dfe690f4e
khigasa <kojihigasa@gmail.com>
2019-05-23 23:55:46 +0900

    Thu 23 May 2019 11:55:46 PM JST

--- echo/echo/target/debug/deps/echo-0193e04c990bc95d.d
+++
@@ -1,5 +0,0 @@
-/home/khigasa/src/github.com/kHigasa/rustcli/echo/echo/target/debug/deps/echo-0193e04c990bc95d: src/main.rs
-
-/home/khigasa/src/github.com/kHigasa/rustcli/echo/echo/target/debug/deps/echo-0193e04c990bc95d.d: src/main.rs
-
-src/main.rs:
--- echo/echo/target/debug/echo.d
+++
@@ -1,1 +0,0 @@
-/home/khigasa/src/github.com/kHigasa/rustcli/echo/echo/target/debug/echo: /home/khigasa/src/github.com/kHigasa/rustcli/echo/echo/src/main.rs
